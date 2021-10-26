8431e90f142c2e057bcf4f2fbfd760ffdc368635
Pavel <kaptowka1999@mail.ru>
2021-10-26 03:06:58 +0300

    added content untill footer

---
+++ assets/js/yandexmap.js
@@ -0,0 +1,7 @@
+ymaps.ready(function(){
+	// Указывается идентификатор HTML-элемента.
+	var moscow_map = new ymaps.Map("map", {
+		center: [53.86377384112042,27.473423473571764],
+		zoom: 15
+	});
+});
